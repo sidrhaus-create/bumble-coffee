ea4ffd9a9161180267d0f5ec1b678db94f4edebf
sidrhaus-create <sidrhaus@gmail.com>
2026-08-28 10:40:32 +0300

    Load Cider House widget safely after Bumble runtime

--- bumble.js
+++ bumble.js
@@ -1,2 +1,14 @@
 /* Loader wrapper: keeps original Bumble runtime intact, then adds Cider House family widget. */
-document.write('<script src="./bumble-core.js"><\/script><script src="./ch-family-widget.js"><\/script>');
+document.write('<script src="./bumble-core.js"><\/script>');
+(function(){
+  function loadWidget(){
+    if(document.getElementById('ch-family-widget-script')) return;
+    var s=document.createElement('script');
+    s.id='ch-family-widget-script';
+    s.src='./ch-family-widget.js?v=2';
+    s.defer=true;
+    document.head.appendChild(s);
+  }
+  if(document.readyState==='loading') document.addEventListener('DOMContentLoaded',loadWidget,{once:true});
+  else loadWidget();
+})();
